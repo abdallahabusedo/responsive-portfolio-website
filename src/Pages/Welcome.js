93c6148ad625e9e57804fac84be4532bea443fef
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2020-05-11 01:16:49 +0200

    🕶 Add Navbar

---
+++ src/Pages/Welcome.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Navbar from './../Component/Navbar';
+export default class Welcome extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
+  }
+}
