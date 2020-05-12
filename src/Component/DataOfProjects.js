bfbc4b40752b6a4163ca7124bddfcfeacb04cfb0
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2020-05-12 07:53:05 +0200

    Add the data of each project

---
+++ src/Component/DataOfProjects.js
@@ -0,0 +1,39 @@
+import calculator from './../assets/Images/calculator.png';
+import Cluc from './../assets/Images/calculator.png';
+import Space from './../assets/Images/Space.png';
+import PacMan from './../assets/Images/pacman (2).png';
+
+import Oud from './../assets/Images/oud.png';
+// const calculator = require('./../assets/Images/calculator.png');
+const info = [
+  {
+    id: 1,
+    name: 'Oud',
+    disc:
+      'Oud is an online music streaming service which is a mimic of Spotify with all its functionalities I was a part of a very good team in this project .',
+    img: Oud,
+  },
+  {
+    id: 2,
+    name: 'calculator',
+    disc:
+      ' This is My First Project in JavaScript , i designed a calculator that have the 4 operations and have in the back a simple linear-gradient animation',
+    img: Cluc,
+  },
+  {
+    id: 3,
+    name: 'Sky invaders',
+    disc:
+      'Sky invaders was a collage project in the graphics design , this game made to destroy the bad enemies in the universe.',
+    img: Space,
+  },
+  {
+    id: 4,
+    name: 'PacMan',
+    disc:
+      '     PacMan well known game from 90s , this project was a self study project in game development using unity',
+    img: PacMan,
+  },
+];
+
+export {info};
