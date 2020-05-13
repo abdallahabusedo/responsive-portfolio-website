aa90f38f22a19fad67d30392c8f8f124be538c37
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2020-05-13 05:54:18 +0200

    Edit the contant

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, {useState, useEffect, Component} from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import ProjectDetails from './Pages/ProjectDetails';
 import Welcome from './Pages/Welcome';
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Route exact path="/">
-          <Welcome />
-        </Route>
-        <Route path="/projects">
-          <ProjectDetails />
-        </Route>
-      </div>
-    </Router>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ThemeMode:
+        localStorage.getItem('theme') !== null
+          ? localStorage.getItem('theme')
+          : true,
+    };
+  }
+
+  componentDidMount() {
+    document.documentElement.setAttribute(
+      'data-theme',
+      this.state.themeMode === true ? 'dark' : 'light'
+    );
+  }
+  handleStoreTheme = () => {};
+  handelThemeOnChange = () => {
+    this.setState({themeMode: !this.state.themeMode});
+    document.documentElement.setAttribute(
+      'data-theme',
+      this.state.themeMode === true ? 'dark' : 'light'
+    );
+    localStorage.setItem('theme', this.state.ThemeMode === true ? true : false);
+  };
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Route exact path="/">
+            <Welcome
+              themeMode={this.state.themeMode}
+              handelThemeOnChange={this.handelThemeOnChange}
+            />
+          </Route>
+          <Route path="/projects">
+            <ProjectDetails
+              themeMode={this.state.themeMode}
+              handelThemeOnChange={this.handelThemeOnChange}
+            />
+          </Route>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import My from './../assets/Images/Whw.jpeg';
 export default class Contact extends Component {
   constructor(props) {
     super(props);
@@ -12,38 +12,22 @@ export default class Contact extends Component {
 
   render() {
     return (
-      <div className="ContantSec" id="contact">
-        <form
-          className="FormSec container"
-          action="mailto:abozoooz2014.aa@gmail.com"
-        >
-          <div>
-            <p className="ContText">Contact Me</p>
-            <p className="ContsText">Name</p>
-            <input
-              type="text"
-              placeholder="Enter your Name"
-              className="Forminp"
-            ></input>
-            <p className="ContsText">Email</p>
-            <input
-              type="email"
-              className="Forminp"
-              placeholder="Enter your Email"
-            ></input>
-            <p className="ContsText">Massage</p>
-            <textarea
-              type="text"
-              className="Forminp"
-              placeholder="Enter your Massage"
-            ></textarea>
-
-            <button type="submit" class="btn" >
-              Send Massage
-            </button>
-            <div className="SocialIcons"></div>
-          </div>
-        </form>
+      <div className="container">
+        <div className="ContantSec" id="contact">
+          <form
+            className="divSec container"
+            action="mailto:abozoooz2014.aa@gmail.com"
+          >
+            <div>
+              <p className="ContText container SecText ">Contact Me</p>
+              <p className=" container SecText">I would love to talk to you </p>
+              <p className=" container SecText">so just send me a Massage</p>
+              <button type="submit" class="btn">
+                Send Massage
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
