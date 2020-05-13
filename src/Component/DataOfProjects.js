380f006640830e541271e366fb27aa2079926d10
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2020-05-13 05:54:56 +0200

    Add the data of the projects

--- src/Component/DataOfProjects.js
+++ src/Component/DataOfProjects.js
@@ -2,16 +2,24 @@ import calculator from './../assets/Images/calculator.png';
 import Cluc from './../assets/Images/calculator.png';
 import Space from './../assets/Images/Space.png';
 import PacMan from './../assets/Images/pacman (2).png';
-
-import Oud from './../assets/Images/oud.png';
+import image3 from './../assets/Images/image3.jpeg';
+import Oud from './../assets/Images/806.png';
+import calcas from './../assets/Images/calcas.jpeg';
+import pacpic from './../assets/Images/pac.png';
+import oudg from './../assets/Images/ez.gif';
+import picgif from './../assets/Images/pacgif.gif';
+import skygif from './../assets/Images/sky.gif';
 // const calculator = require('./../assets/Images/calculator.png');
 const info = [
   {
     id: 1,
     name: 'Oud',
     disc:
-      'Oud is an online music streaming service which is a mimic of Spotify with all its functionalities I was a part of a very good team in this project .',
+      'Oud is an online music streaming service which is a mimic of Spotify with all its functionalities I was a part of a very good team in this project . and this is one of feature the suggested artist after signup  ',
     img: Oud,
+    imagt: image3,
+    gif: oudg,
+    href: 'https://github.com/abdallahabusedo/oudFrontend',
   },
   {
     id: 2,
@@ -19,6 +27,9 @@ const info = [
     disc:
       ' This is My First Project in JavaScript , i designed a calculator that have the 4 operations and have in the back a simple linear-gradient animation',
     img: Cluc,
+    imagt: calcas,
+    gif: calcas,
+    href: 'https://github.com/abdallahabusedo/web-calculator',
   },
   {
     id: 3,
@@ -26,13 +37,19 @@ const info = [
     disc:
       'Sky invaders was a collage project in the graphics design , this game made to destroy the bad enemies in the universe.',
     img: Space,
+    imagt: '',
+    gif: skygif,
+    href: 'https://github.com/abdallahabusedo/sky-invaders-',
   },
   {
     id: 4,
     name: 'PacMan',
     disc:
-      '     PacMan well known game from 90s , this project was a self study project in game development using unity',
+      'PacMan well known game from 90s , this project was a self study project in game development using unity',
     img: PacMan,
+    imagt: pacpic,
+    gif: picgif,
+    href: 'https://github.com/abdallahabusedo/pacman-c-sharp',
   },
 ];
 
